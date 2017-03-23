1c68492eb81f7b2243622e4fdc9074d87deebc02
tiwanari <tatsuyaw0c@gmail.com>
2017-03-23 18:10:07 +0900

    Add BluetoothCommunicator

---
+++ android/app/src/main/java/com/pileproject/drivecommand/examples/android/BluetoothCommunicator.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (C) 2011-2017 The PILE Developers <pile-dev@googlegroups.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pileproject.drivecommand.examples.android;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.pileproject.drivecommand.model.com.ICommunicator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+
+/**
+ * An implementation of {@link ICommunicator} of the <code>DriveCommand</code> plugin. This enables Android devices
+ * to communicate with machines via Bluetooth.
+ *
+ * @see <a href="https://github.com/PileProject/drivecommand">DriveCommand</a>
+ */
+public class BluetoothCommunicator implements ICommunicator {
+    private static final UUID SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final String TAG = "BluetoothCommunicator";
+
+    private BluetoothDevice mDevice;
+    private BluetoothSocket mSocket;
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+
+    public BluetoothCommunicator(@NonNull BluetoothDevice device) throws NullPointerException {
+        if (device == null) {
+            throw new NullPointerException("Device should not be null");
+        }
+        mDevice = device;
+    }
+
+    @Override
+    public void open() throws IOException {
+        // NOTE: an orthodox method
+        // it depends on the device if this call fails or not
+        // so, we do a redundancy check with the below reflection method
+        mSocket = mDevice.createRfcommSocketToServiceRecord(SPP_UUID);
+        try {
+            mSocket.connect();
+        } catch (IOException firstIOException) {
+            Log.d(TAG, "Failed to connect with an orthodox method");
+            try {
+                // a redundancy check
+                Method method = mDevice.getClass().getMethod("createRfcommSocket", int.class);
+                mSocket = (BluetoothSocket) method.invoke(mDevice, 1);
+                mSocket.connect();
+            } catch (IOException secondIOException) {
+                Log.d(TAG, "Failed to connect with a redundancy method");
+                // unable to connect; close the socket and get out
+                try {
+                    mSocket.close();
+                } catch (IOException closeException) {
+                    closeException.printStackTrace();
+                    Log.d(TAG, "It seems unable to recover");
+                }
+                throw secondIOException;
+            } catch (Exception exception) {
+                exception.printStackTrace();
+                Log.d(TAG, "This exception should not be occurred in release version");
+            }
+        }
+
+        mOutputStream = mSocket.getOutputStream();
+        mInputStream = mSocket.getInputStream();
+    }
+
+    @Override
+    public void close() {
+        if (mSocket != null) {
+            try {
+                mSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to close connection", e);
+            }
+        }
+        mSocket = null;
+    }
+
+    @Override
+    public void write(byte[] request) throws RuntimeException {
+        try {
+            mOutputStream.write(request);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] read(int length) throws RuntimeException {
+        byte[] buffer = new byte[length];
+        int numBytes;
+        try {
+            numBytes = mInputStream.read(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] result = new byte[numBytes];
+        System.arraycopy(buffer, 0, result, 0, numBytes);
+
+        return result;
+    }
+}
